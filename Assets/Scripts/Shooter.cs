8ae4bd9983caec52bb0f24f5bbf2b3c28b53ef17
kinjo <eyjgdknc@gmail.com>
2023-02-08 14:52:16 +0900

    p216

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    const int MaxShotPower = 5;
+    const int RecoverySeconds = 3;
+
+    int shotPower = MaxShotPower;
+
     public GameObject[] candyPrefabs;
     public Transform candyParentTransform;
     public CandyManager candyManager;
@@ -34,6 +39,7 @@ public class Shooter : MonoBehaviour
     {
         //キャンディを生成できる条件外ならばShotしない
         if (candyManager.GetCandyAmount() <= 0) return;
+        if (shotPower <= 0) return;
 
         //プレハブからCandyオブジェクトを生成
         GameObject candy = (GameObject)Instantiate(
@@ -53,5 +59,31 @@ public class Shooter : MonoBehaviour
 
         //Candyのストックを消費
         candyManager.ConsumeCandy();
+        //ShotPowerを消費
+        ConsumePower();
+    }
+
+    void OnGUI() {
+
+            GUI.color = Color.black;
+
+            //ShotPowerの残数を+の数で表示
+            string label = "";
+            for (int i = 0; i < shotPower; i++) label = label + "+";
+
+            GUI.Label(new Rect(50, 65, 100,30), label);
+    }
+
+    void ConsumePower()
+    {
+        //ShotPowerを消費すると同時に回復のカウントをスタート
+        shotPower--;
+        StartCoroutine(RecoverPower());
+    }
+    IEnumerator RecoverPower()
+    {
+        //一定秒数待った後にshotPowerを回復
+        yield return new WaitForSeconds(RecoverySeconds);
+        shotPower++;
     }
 }
